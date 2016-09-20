59993ce5ad54a157f3f40ee8d0283f5e80a298c7
Elana Kopelevich <ekopelevich@gmail.com>
2016-09-20 11:27:38 -0600

    knex setup

---
+++ knexfile.js
@@ -0,0 +1,10 @@
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: 'postgres://localhost/blog_db',
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL + '?ssl=true',
+  },
+}
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 var express = require('express')
 var router = express.Router()
+var db = require('../db/api')
 
 router.get('/', function(req, res) {
   res.send('this is the users route')
